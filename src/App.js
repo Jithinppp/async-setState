91f72398ba56400625b29ff797f5e76a69c1c5aa
unknown <jithinp968@gmail.com>
2021-10-13 19:34:39 +0530

    method that async-setState used to manipulate states both good and bad practices

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    //if you want to use the props in the constructor use props arg as in both super and contructor
+    //its not nessary to use constructor and  super to declate the state  if we need  propsthen its nessary
+
+    this.state = {
+      number: 20,
+    };
+  }
+
+  HandleClick1 = () => {
+    this.setState({ number: this.state.number + 1 });
+    console.log(this.state.number);
+    //this will be one behind the state to overcome this use a callback function in the setState
+    //like this   this.setState({ number: this.state.number + 1 },()=> console.log(this.state.number));
+    //this is also a bad practice becusereact doesn't guarentee the changes maybe sometimes
+    //its  fine in the case of only  string operations like this.setState(number: 'strings')
+  };
+
+  //to overcome this issue use functions in the setState
+  //example handleClick = ()=>{ this.setState((prevState,prevProps)=>(clg('anything'))) }
+
+  HandleClick2 = () => {
+    this.setState(
+      (prevState, prevProps) => {
+        return { number: prevState.number + prevProps };
+        //this prevState will give the state value and this prevProps give props from the upper node
+      },
+      () => {
+        console.log(this.state.number);
+      }
+    );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.number}</p>
+
+          <button onClick={this.HandleClick2}>update State</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
